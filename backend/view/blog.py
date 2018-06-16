719055840fe04368e757bbd17cc6ffce5a502534
杨小事er <Uiryzd@163.com>
2018-06-16 20:31:10 +0800

    fix(blog)

--- backend/view/blog.py
+++ backend/view/blog.py
@@ -12,7 +12,7 @@ from django.shortcuts import render_to_response
 from django.template import loader,Context
 
 from backend.models import BlogsPost
-
+from django.utils import timezone
 
 #  获取user
 from django.contrib.auth.models import User
@@ -32,13 +32,13 @@ from django.http import StreamingHttpResponse
 
 
 def get_all_blogs(request):
-    blog_querySet = BlogsPost.objects.all()
+    blog_querySet = BlogsPost.objects.all().order_by('-id')
 
     request_dict = json.loads(request.body.decode())
 
     blog_list = list(blog_querySet.values())
-    print(request_dict["page"])
-    print(request_dict["size"])
+
+    print("page:" + str(request_dict["page"]) + ",size: "+str(request_dict["size"]))
 
     res_page = request_dict["page"]
     res_size = request_dict["size"]
@@ -66,10 +66,10 @@ def blog_list2json(blog_list):
             "id": value["id"],
             "title": value["title"],
             "content": value["content"],
-            "create_time": value["create_time"].strftime('%Y-%m-%d %H:%M:%S'),
-            "modify_time": value["modify_time"].strftime('%Y-%m-%d %H:%M:%S'),
+            "create_time": timezone.localtime(value["create_time"]).strftime('%Y-%m-%d %H:%M:%S'),
+            "modify_time": timezone.localtime(value["modify_time"]).strftime('%Y-%m-%d %H:%M:%S'),
             "views": value["views"],
-            "timestamp": value["timestamp"].strftime('%Y-%m-%d %H:%M:%S'),
+            "timestamp": timezone.localtime(value["timestamp"]).strftime('%Y-%m-%d %H:%M:%S'),
             # "tag": value["tag"],
             "classification": value["classification"],
             "description": value["description"],
@@ -92,9 +92,9 @@ def get_blog_by_id(request):
     else:
         blog_dict = list(blog_querySet.values())[0]
     # blog_dict = model_to_dict(blog_querySet)
-    blog_dict["timestamp"] = blog_dict["timestamp"].strftime('%Y-%m-%d %H:%M:%S')
-    blog_dict["modify_time"] = blog_dict["modify_time"].strftime('%Y-%m-%d %H:%M:%S')
-    blog_dict["create_time"] = blog_dict["create_time"].strftime('%Y-%m-%d %H:%M:%S')
+    blog_dict["timestamp"] = timezone.localtime(blog_dict["timestamp"]).strftime('%Y-%m-%d %H:%M:%S')
+    blog_dict["modify_time"] = timezone.localtime(blog_dict["modify_time"]).strftime('%Y-%m-%d %H:%M:%S')
+    blog_dict["create_time"] = timezone.localtime(blog_dict["create_time"]).strftime('%Y-%m-%d %H:%M:%S')
     blog_dict["tags"] = ''
     print(blog_dict)
 
